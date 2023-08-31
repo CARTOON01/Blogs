6646b440a5a024721e0b24c11b9bc8354308e205
CARTOON01 <brianngugi224@gmail.com>
2023-08-31 11:56:39 +0300

    public

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,20 @@ import preprocessor from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
-	preprocess: preprocessor({
-		scss: {
-			prependData: `@use './src/styles/variables.scss' as *;`
-		}
+  preprocess: preprocessor({
+    scss: {
+      prependData: `@use './src/styles/variables.scss' as *;`
+    }
 	}),
-	kit: {
-		adapter: adapter({
-			edge: false,
-			split: false,
-			compress: true
-		})
-	}
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false,
+      compress: true,
+      // Use the "public" directory as the output
+      out: 'public'
+    })
+  }
 };
 
 export default config;
